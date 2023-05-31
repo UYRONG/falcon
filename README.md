48b175f0433b0a5a2ee1995f099c7c26f42d35d6
Zoraaaa <rongyu923826@outlook.com>
2023-05-31 13:09:51 -0700

    Updated README for dependency package, simple script for running simple example in falcon

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # falcon
-falcon
+
+Dependency:
+
+# Transformers installation
+# ! pip install transformers datasets
+pip install torch
+pip install einops
+pip install accelerate
+pip install transformers==4.27.4
+pip install huggingface-hub
+pip install chardet
+pip install cchardet
+
+https://colab.research.google.com/drive/1B7XTd31mVEPgkqgCi0Kg0Z0XD-mA1GDC?usp=sharing
---
+++ falcon_7b.py
@@ -0,0 +1,31 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+model = "tiiuae/falcon-7b"
+# model = "tiiuae/falcon-40b"
+
+rrmodel = AutoModelForCausalLM.from_pretrained(model, 
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+    offload_folder="offload")
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+input_text = "I want all books and you get the rest"
+input_ids = tokenizer.encode(input_text, return_tensors='pt')
+
+attention_mask = torch.ones(input_ids.shape)
+
+output = rrmodel.generate(input_ids, 
+            attention_mask=attention_mask, 
+            max_length=200,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,)
+
+output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+print(output_text)
