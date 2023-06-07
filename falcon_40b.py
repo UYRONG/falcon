190587a65e8a609deb774d9ac441c7128ade0d29
Zoraaaa <rongyu923826@outlook.com>
2023-06-06 17:47:22 -0700

    updated prompt

--- falcon_40b.py
+++ falcon_40b.py
@@ -40,7 +40,7 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(model)
 
     file1 = open('prompt.txt', 'r')
-    Lines = file1.readlines()[1:]
+    Lines = file1.readlines()
 
     count = 0
 
