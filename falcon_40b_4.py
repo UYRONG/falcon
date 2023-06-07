f5e501307688d59d91c323d3f548426cc1530047
Zoraaaa <rongyu923826@outlook.com>
2023-06-06 18:33:58 -0700

    create seperate slurms files for testing

---
+++ falcon_40b_4.py
@@ -0,0 +1,54 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+def generate(input_text,rrmodel,tokenizer):
+    print("----"*10)
+    print("Input:")
+    print(input_text)
+    print("----"*10)
+
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+
+    attention_mask = torch.ones(input_ids.shape)
+
+    output = rrmodel.generate(input_ids, 
+                attention_mask=attention_mask, 
+                max_length=300,
+                do_sample=True,
+                top_k=10,
+                num_return_sequences=1,
+                eos_token_id=tokenizer.eos_token_id,)
+
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    print("Output:")
+    print(output_text)
+    print("----"*10)
+
+
+def main():
+    model = "tiiuae/falcon-40b"
+
+    rrmodel = AutoModelForCausalLM.from_pretrained(model, 
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+        offload_folder="offload",
+        cache_dir="/scratch1/yrong016")
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+    file1 = open('prompt.txt', 'r')
+    Lines = file1.readlines()[16:18]
+
+    count = 0
+
+    for line in Lines:
+        count += 1
+        input_text = line.strip()
+        print("EXAMPLE " + str(count) + ":")
+        generate(input_text,rrmodel,tokenizer)
+
+if __name__ == "__main__":
+    main()
